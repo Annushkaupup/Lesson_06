84ef9948b8867c3f7cd16f51f0b2e19c7497390a
Anna Starikova <veselova666@gmail.com>
2022-04-27 12:12:14 +0300

    exs_05_if else

---
+++ exs_05_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марли")
+{
+    Console.WriteLine("Извольте кушать, Марли");
+}
+else
+{
+    Console.Write("Здравствуйте, ");
+    Console.WriteLine(username);
+}
